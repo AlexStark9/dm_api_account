70495f51120b395005c7b501c22225fb3ff4188b
AlexStark <alex.stark3038@gmail.com>
2023-08-03 13:11:43 +0300

    Добавил пакет для dm_api_account Правки

--- apis/account_api.py
+++ apis/account_api.py
@@ -1,8 +1,8 @@
 import allure
 from requests import Response
-from dm_api_account.models import *
+from models import *
 from rest_client.rest_client import Restclient
-from dm_api_account.utilities import validate_request_json, validate_status_code
+from utilities import validate_request_json, validate_status_code
 
 
 class AccountApi:
